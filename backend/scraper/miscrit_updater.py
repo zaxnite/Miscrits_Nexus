a892a4d37dd0a62266fc1f9b7f00033576ce9865
Aathif Khan <aathif753@gmail.com>
2025-07-26 17:31:08 +0400

    Updated Miscrit Db
    All the miscrits are updated according to the new patch

--- backend/scraper/miscrit_updater.py
+++ backend/scraper/miscrit_updater.py
@@ -12,15 +12,7 @@ miscrit_df = pd.read_csv(miscrit_path)
 moves_df = pd.read_csv(moves_path)
 
 #miscrits= ['Vhisp', 'Zappup', 'Dark Breezycheeks', 'Dark Lumera', 'Light Weevern', 'Light Grubbean', 'Boltzee','Beelzebug', 'Flaring', 'Gemix', 'Shurikoon', 'Light Snorkels', 'Dark Poltergust', 'Light Ignios', 'Splender', 'Dark Snortus', 'Foil Croaky', 'Sol', 'Podo', 'Inferno', 'Freedom', 'Valentino', 'Ekkult', 'Alpha', 'Nanaslug', 'Shellbee', 'Statikat', 'Aria', 'Luna', 'Hippoke', 'Leggy', 'Whik', 'Hawkai', 'Nibbles', 'Crabbles', 'Rafiery', 'Squirmle', 'Peekly', 'Dark Shellbee', 'Dark Aria', 'Light Sparkspeck', 'Light Hawkai', 'Dark Vexie', 'Waddles']
-
-
-#miscrits= ['Shellbee', 'Statikat', 'Aria', 'Luna', 
-# 'Hippoke', 'Leggy', 'Whik', 'Hawkai', 'Nibbles', 'Crabbles', 'Rafiery', 'Squirmle', 'Peekly', 'Dark Shellbee', 'Dark Aria', 
-# 'Light Sparkspeck', 'Light Hawkai', 'Dark Vexie', 'Waddles']
-
-
-miscrits = ['Podo', 'Inferno', 'Freedom', 'Valentino', 'Ekkult', 'Alpha', 'Nanaslug']
-
+miscrits =[]
 
 
 def miscrit_id_finder(miscrits: list) -> str:
